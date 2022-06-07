ddead9cb5a390b4477d66e2762ff09ff86340d9a
Luis Martínez Vicente <4683958@alu.murciaeduca.es>
2022-06-07 18:51:55 +0200

    Form entrenos

--- src/Paginas/Entrenamientos/Entrenamientos.js
+++ src/Paginas/Entrenamientos/Entrenamientos.js
@@ -10,9 +10,6 @@ import galeria2 from "../../imagenes/Fotos wpt/ari2.jpg";
 import galeria3 from "../../imagenes/Fotos wpt/ale4.jpg";
 import flechas from "../../imagenes/flechas.png";
 
-import { Form } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-
 function Entrenamientos() {
 
     return (
@@ -60,7 +57,7 @@ function Entrenamientos() {
                         <div className="row">
                             <div className="col-6 align-self-center">
                                 <h1 className="textorojo">Posicionamiento</h1>
-                                <h5>La colocación dentro de la pista es crucial a la hora de realizar golpes complicados o hacer menos esfuerzos. Para realizar un buen resto, hacer una volea ganadora, 
+                                <h5>La colocación dentro de la pista es crucial a la hora de realizar golpes complicados o hacer menos esfuerzos. Para realizar un buen resto, hacer una volea ganadora,
                                     saltar hacia un remate o devolverlo, es muy importante estar bien posicionado. La colocación en pista te hará ganar ventaja respecto a tus rivales.</h5>
                             </div>
                             <div className="col-6">
@@ -81,7 +78,7 @@ function Entrenamientos() {
                             </div>
                             <div className="col-6 align-self-center">
                                 <h1 className="textorojo">Trabajo</h1>
-                                <h5>No hay éxito sin sacrificio. Los partidos son largos y siempre hay altibajos, pero la clave está en seguir trabajando con esfuerzo. 
+                                <h5>No hay éxito sin sacrificio. Los partidos son largos y siempre hay altibajos, pero la clave está en seguir trabajando con esfuerzo.
                                     Lucha cada bola como si fuera la última y consigue dar tu mejor versión en la pista.</h5>
                             </div>
 
@@ -93,9 +90,9 @@ function Entrenamientos() {
                             <div className="col-12 pt-4 pb-4">
                                 <div className="row">
                                     <h1 className="textorojo text-center pb-4">Revisión de vídeo</h1>
-                                    <h4 className="text-center">Trabajar con sesiones de vídeo hará que te sea más fácil afianzar los conceptos que entrenes en la pista. Tus sesiones de 
-                                    entrenamiento serán grabadas con el objetivo de comentarlas con el entrenador y ver fácilmente que se puede mejorar. Este método acompañará a tu trabajo 
-                                    en pista para ir mejorando en todo momento y comparar con sesiones anteriores para comprobar tu progreso.
+                                    <h4 className="text-center">Trabajar con sesiones de vídeo hará que te sea más fácil afianzar los conceptos que entrenes en la pista. Tus sesiones de
+                                        entrenamiento serán grabadas con el objetivo de comentarlas con el entrenador y ver fácilmente que se puede mejorar. Este método acompañará a tu trabajo
+                                        en pista para ir mejorando en todo momento y comparar con sesiones anteriores para comprobar tu progreso.
                                     </h4>
                                 </div>
                             </div>
@@ -129,26 +126,28 @@ function Entrenamientos() {
                         <div className="row pt-10">
                             <h1 className="textorojo text-center">Nosotros ponemos el contexto, tú eres el protagonista</h1>
                             <h3 className="text-white text-center">Si te atreves a vivir esta experiencia o necesitas más información, contacta con nosotros</h3>
-                        
-                            <Form>
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className="text-white">Nombre</Form.Label>
-                                    <Form.Control type="email" placeholder="Introduce tu nombre" />
-                                </Form.Group>
 
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                    <Form.Label className="text-white">Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Introduce tu email" />
-                                </Form.Group>
+                            <form className="text-white">
+                                <div className="form-group mb-3">
+                                    <label>Nombre</label>
+                                    <input type="" name="name" placeholder="Introduce tu nombre" className="form-control" />
+                                    <span>{}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label>Email</label>
+                                    <input type="" name="email" placeholder="Introduce tu email" className="form-control" />
+                                    <span>{}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label>Mensaje</label>
+                                    <textarea rows="3" name="mensaje" className="form-control" />
+                                    <span>{}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <button type="submit" className="btn btn-light custom-btn">Enviar</button>
+                                </div>
+                            </form>
 
-                                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                    <Form.Label className="text-white">Mensaje</Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
-                                </Form.Group>
-
-                                
-                                <button type="button" className="btn btn-light custom-btn">Enviar</button>
-                            </Form>
                         </div>
                     </div>
                 </div>
